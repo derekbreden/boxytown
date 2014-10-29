d2fce14d071fca8c97b3d9fd752926e72986ccb9
Derek Bredensteiner <derekbreden@gmail.com>
2014-10-29 15:12:37 -0700

    styling

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express()
 var redis = require('redis')
 var client = redis.createClient()
 var compress = require('compression')
+var session = require('express-session')
 
+app.use(session({secret: 'keyboard cat'}))
 app.use(compress())
 app.use(express["static"](__dirname + '/public'))
 
