4795d343aa70234a5fe3fdbeaf8ab9c6c8d52c0b
Derek Bredensteiner <derekbreden@gmail.com>
2014-10-29 16:40:41 -0700

    progress

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var client = redis.createClient()
 var compress = require('compression')
 var session = require('express-session')
 var RedisStore = require('connect-redis')(session)
+var bodyParser = require('body-parser')
 
 app.use(session({
   store: new RedisStore({}),
   secret: 'keyboard cat'
 }))
 app.use(compress())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use(express["static"](__dirname + '/public'))
 
 app.all('/u', function(req, res){
@@ -33,6 +36,39 @@ app.get('/new', function(req, res){
 app.get('/:which/edit', function(req, res){
   res.sendFile(__dirname + '/public/edit.html') })
 
+app.get('/:which', function(req, res){
+  var w = req.params.which
+  client.get('contents:'+w,function(err, reply){
+    if(blowup(err,res)){ return }
+    res.send(reply || "")
+  })
+})
+
+app.post('/:which/save', function(req, res){
+  var w = req.params.which
+  client.get('session:'+w, function(err, session_reply){
+    if(blowup(err,res)){ return }
+    client.get('password:'+w, function(err, password_reply){
+      if(blowup(err,res)){ return }
+      if(
+        (session_reply && req.session.sessionId === session_reply)
+        ||
+        (password_reply && req.session.password === password_reply)
+        ||
+        (!session_reply)
+      ){
+        client.set("contents:" + w, req.body.contents || " ", function(err, reply){
+          if(blowup(err,res)){ return }
+          if(!session_reply){
+            client.set("session:"+w,req.session.sessionId,function(){}) }
+          res.send("OK")
+        })
+      }else{
+        res.send("NOT OK")
+      }
+    })
+  })
+})
 
 var port = process.env.PORT || process.env.HTTP_PORT || 8000
 app.listen(port)
