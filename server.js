fd2d6c59776e7ab4f8368a40bba6c1841e3a518e
Derek Bredensteiner <derekbreden@gmail.com>
2014-10-30 00:26:33 -0700

    Fix iframe

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(function(req, res, next){
     res.send('\
     <!doctype html>\
     <style type="text/css">\
-      body{ margin: 0 }\
-      iframe{ position: absolute;\
+      html,body,iframe{ position: absolute;\
+      display: block; width: 100%; height: 100%;\
       top: 0; left: 0; right: 0; bottom: 0;\
       border: 0; margin: 0; padding: 0;}\
     </style>\
